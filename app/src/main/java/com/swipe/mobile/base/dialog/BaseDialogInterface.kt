7676134e0369a00ad1461c79fe6f0946f3d196a3
anonGTF <anongtf26@gmail.com>
2022-11-21 22:54:44 +0800

    slicing layout

--- app/src/main/java/com/swipe/mobile/base/dialog/BaseDialogInterface.kt
+++
@@ -1,7 +0,0 @@
-package com.swipe.mobile.base.dialog
-
-
-interface BaseDialogInterface {
-    fun onSubmitClick()
-    fun onDismissClick()
-}
--- app/src/main/java/com/swipe/mobile/base/recyclerview/EndlessScrollCallback.kt
+++
@@ -1,6 +0,0 @@
-package com.swipe.mobile.base.recyclerview
-
-//Callback only provides load more for now
-interface EndlessScrollCallback{
-    fun loadMore()
-}
